1b604ee6d3c58433c1b5b4f429a3aff26bf95a4f
Yutokara <ota.se.ke@outlook.jp>
2024-11-28 00:04:22 +0900

    second commit

---
+++ src/components/layout/common/Layout/common/Sidebar.tsx
@@ -0,0 +1,111 @@
+import {
+  Box,
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+} from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import EqualizerIcon from '@mui/icons-material/Equalizer';
+import { NavLink } from 'react-router-dom';
+import { CSSProperties } from 'react';
+
+interface SidebarProps {
+  drawerWidth: number;
+  mobileOpen: boolean;
+  handleDrawerClose: () => void;
+  handleDrawerTransitionEnd: () => void;
+} //typeの方が定義できる方の種類が多い
+
+interface menuItem {
+  text: string;
+  path: string;
+  icon: React.ComponentType;
+}
+
+const Sidebar = ({ drawerWidth, mobileOpen, handleDrawerClose, handleDrawerTransitionEnd }: SidebarProps) => {
+  const MenuItems: menuItem[] = [
+    { text: 'Home', path: '/', icon: HomeIcon },
+    { text: 'Report', path: '/report', icon: EqualizerIcon },
+  ];
+
+  const baseLinkStyle: CSSProperties = {
+    textDecoration: 'none',
+    color: 'inherit',
+    display: 'block',
+  };
+
+  const activeLinkStyle: CSSProperties = {
+    backgroundColor: 'rgba(0, 0, 0, 0.08)',
+  };
+
+  const drawer = (
+    <div>
+      <Toolbar />
+      <Divider />
+      <List>
+        {MenuItems.map((item, index) => (
+          <NavLink
+            key={item.text} //ユニークなキー、indexでもOK!
+            to={item.path}
+            style={({ isActive }) => {
+              console.log('選択されたメニューは', item.text, isActive);
+              return {
+                ...baseLinkStyle, //これだけで展開できる
+                ...(isActive ? activeLinkStyle : {}),
+              };
+            }}
+          >
+            <ListItem key={index} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {/* {index % 2 === 0 ? <InboxIcon /> : <MailIcon />} */}
+                  <item.icon />
+                </ListItemIcon>
+                <ListItemText primary={item.text} />
+              </ListItemButton>
+            </ListItem>
+          </NavLink>
+        ))}
+      </List>
+    </div>
+  );
+
+  return (
+    <Box component='nav' sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }} aria-label='mailbox folders'>
+      {/* モバイル用 */}
+      <Drawer
+        variant='temporary'
+        open={mobileOpen}
+        onTransitionEnd={handleDrawerTransitionEnd}
+        onClose={handleDrawerClose}
+        ModalProps={{
+          keepMounted: true, // Better open performance on mobile.
+        }}
+        sx={{
+          display: { xs: 'block', sm: 'none' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+        }}
+      >
+        {drawer}
+      </Drawer>
+      {/* PC用 */}
+      <Drawer
+        variant='permanent'
+        sx={{
+          display: { xs: 'none', sm: 'block' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+        }}
+        open
+      >
+        {drawer}
+      </Drawer>
+    </Box>
+  );
+};
+
+export default Sidebar;

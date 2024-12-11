70cef34b73a28109d593fc6c5319cb7da6066f44
Yutokara <ota.se.ke@outlook.jp>
2024-12-12 01:20:27 +0900

    68から

--- src/components/layout/BarChart.tsx
+++ src/components/layout/BarChart.tsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import { Bar } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 const BarChart = () => {
-  return <div>BarChart</div>;
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top' as const,
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Bar Chart',
+      },
+    },
+  };
+
+  const labels = ['2024-12-25', '2024-12-25', '2024-12-25', '2024-12-25', '2024-12-25', '2024-12-25', '2024-12-25'];
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: '支出',
+        data: [100, 200, 300, 300, 400, 400, 500],
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+      {
+        label: '収入',
+        data: [100, 200, 300, 300, 400, 400, 500],
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  };
+
+  return <Bar options={options} data={data} />;
 };
 
 export default BarChart;
